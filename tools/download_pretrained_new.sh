9dc592fae81f0845e95013a839940246f8f1c916
Ziba Parsons <zibapars@umich.edu>
2024-04-29 18:15:05 -0400

    Add files via upload
    the pretrained files are removed. A google drive access is provided instead.

---
+++ tools/download_pretrained_new.sh
@@ -0,0 +1,10 @@
+mkdir pretrained &&
+cd pretrained &&
+wget https://drive.google.com/drive/folders/1Jru7VTfgvFF949DlP1oDfriP3wrmOd3c/bevfusion-det.pth &&
+wget https://drive.google.com/drive/folders/1Jru7VTfgvFF949DlP1oDfriP3wrmOd3c/bevfusion-seg.pth &&
+wget https://drive.google.com/drive/folders/1Jru7VTfgvFF949DlP1oDfriP3wrmOd3c/lidar-only-det.pth &&
+wget https://drive.google.com/drive/folders/1Jru7VTfgvFF949DlP1oDfriP3wrmOd3c/lidar-only-seg.pth &&
+wget https://drive.google.com/drive/folders/1Jru7VTfgvFF949DlP1oDfriP3wrmOd3c/camera-only-det.pth &&
+wget https://drive.google.com/drive/folders/1Jru7VTfgvFF949DlP1oDfriP3wrmOd3c/camera-only-seg.pth &&
+wget https://drive.google.com/drive/folders/1Jru7VTfgvFF949DlP1oDfriP3wrmOd3c/swint-nuimages-pretrained.pth
+
